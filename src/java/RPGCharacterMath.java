aafa9d5c164bdb4dcd96b2efdb50764d4592b2ae
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-06-09 17:32:46 +0300

    feat: added Readme

---
+++ src/java/RPGCharacterMath.java
@@ -0,0 +1,8 @@
+
+public class RPGCharacterMath {
+    public static void main(String[] args) {
+        int swordDamage = 5;
+        int strengthBonus = 10;
+
+        System.out.println ("Oбщий урон за удар" + " " + (swordDamage+strengthBonus));
+    }
